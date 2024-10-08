67a6fafe62d88e21653de9d872fc2b43ebffa380
Pimenta Eduardo <T0307760@gemalto.com>
2024-10-08 17:33:13 -0300

    findById implementation

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -8,8 +8,8 @@ public interface DepartmentDao {
 
 	void insert(Department obj);
 	void update(Department obj);
-	void deleteById(Department obj);
-	Department findById(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
 	List<Department> findAll();
 	
 }
