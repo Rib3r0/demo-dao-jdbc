3aac821ef1fdd065c2c1c5add038a49845c32de3
Pimenta Eduardo <T0307760@gemalto.com>
2024-10-08 16:46:41 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,44 +1,13 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import db.DB;
-import db.DbException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = null;
-		PreparedStatement st = null;
-		try {
-			conn = DB.getConnection();
-			
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE "
-					+ "id = ?"
-					);
-
-			
-			st.setInt(1, 2);
-			
-			int rowsAffected = st.executeUpdate();
-			
-			System.out.println("Done! Rows affected: "+ rowsAffected);	
-		}
-		catch (SQLException e) {
-			throw new DbException(e.getMessage());
-		}
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+		
 	}
 }
